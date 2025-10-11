b19073174481df4c82ee8a75100cea90fd02cb62
lucaslinAn <lucaslangelini@gmail.com>
2025-10-11 13:01:20 +0100

    Create skate.py

---
+++ skate.py
@@ -0,0 +1,6 @@
+Skate_Atual =''
+while Skate_Atual!=('Fred Cole'):
+    Skate_Atual=input("Qual skatista vc está vendo?")
+    print("Você está vendo {},".format(Skate_Atual))
+    if Skate_Atual :("Fred Cole")
+    print("Oh,Você é das antigas!!!")
