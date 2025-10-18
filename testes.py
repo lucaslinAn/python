28cac086e14a0f74c89ad6889a8ae9878fd981c5
lucaslinAn <lucaslangelini@gmail.com>
2025-10-18 12:56:31 +0100

    Implement client name collection and display
    Fix issues with appending names and printing.

---
+++ testes.py
@@ -0,0 +1,11 @@
+nomes = []
+clientes = int(input("Digite quantos clientes antendemos: "))
+
+for i in range(clientes):
+    nome = input("Digite o nome: "). capitalize()
+    nomes.append(nomes)
+
+print("\nOs clientes do dia foram: \n")
+
+for nome in nomes:
+    print(nomes)
